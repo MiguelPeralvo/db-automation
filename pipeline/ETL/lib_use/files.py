3d693fa93645614ac1caffd25578c95f818e2aac
miguel.peralvo <miguel.peralvo@databricks.com>
2020-06-10 20:38:51 +0100

    Add exit

--- pipeline/ETL/lib_use/files.py
+++ pipeline/ETL/lib_use/files.py
@@ -6,3 +6,7 @@ from dbxdemo import appendcol
 display(appendcol.with_status(spark.read.parquet("/databricks-datasets/samples/lending_club/parquet/")))
 
 
+# COMMAND ----------
+
+dbutils.notebook.exit(True)
+

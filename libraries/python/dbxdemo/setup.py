ab90618191c63aded1c0dfd42348deab711c1aec
miguel.peralvo <miguel.peralvo@databricks.com>
2020-06-10 20:12:08 +0100

    Start working with libraries.

---
+++ libraries/python/dbxdemo/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='dbxdemo',
+      version='0.0.15',
+      description='A sample PySpark application - 0.0.15',
+      author='Peter Tamisin',
+      author_email='peter.tamisin@databricks.com',
+      url='www.databricks.com',
+      packages=['dbxdemo'],
+      zip_safe=False)
